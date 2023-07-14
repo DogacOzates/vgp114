e6132263cdf75a9ddcaeb19a8573aab897485ab8
DogacOzates <67386086+DogacOzates@users.noreply.github.com>
2023-07-13 21:32:09 -0700

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float Speed = 5f;
+    private Vector2 targetposition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0)) {
+            SetTargetPosition();
+        }
+
+        Move();
+    }
+
+    void SetTargetPosition() {
+        targetposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void Move() {
+
+        Vector2 direction = new Vector2(targetposition.x - transform.position.x, targetposition.y - transform.position.y);
+
+        transform.position = Vector2.MoveTowards(transform.position, targetposition, Speed*Time.deltaTime);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+
+        }
+    }
+
+}
+
+
